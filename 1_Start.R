a6ea59badd8cc06353e78e5744c304fe1839df7f
Muhammad Zabidi <mamduh.zabidi@gmail.com>
2021-09-16 21:14:18 +0800

    Update 1_Start.R

--- 1_Start.R
+++ 1_Start.R
@@ -5,6 +5,10 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 BiocManager::install(version = "3.13")
 
+# try installing some packages
+BiocManager::install(c("GenomicFeatures", "AnnotationDbi"))
+
+
 
 
 ######
